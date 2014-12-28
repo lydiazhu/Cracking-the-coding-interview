55a6be221f3a4451fc5ef1b7eaa2c4d0154146f6
lydiazhu <mxzhu@uwaterloo.ca>
2014-12-28 16:46:04 -0500

    chapter1c

--- chapter1b.cc
+++ chapter1b.cc
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
+//O(1)
 void reverse(char* str){
 	int length = strlen(str);
 	int low = 0;
 	int high = length-1;
-	if (length%2!=0){
-		while (low != high){
+		while ( high - low >= 1){
 			char begin = str[low];
 			char end = str[high];
 			str[low] = end;
@@ -14,17 +14,7 @@ void reverse(char* str){
 			low++;
 			high--;
 		}
-	}
-	else {
-		while(low - high !=1){
-			char begin = str[low];
-			char end = str[high];
-			str[low] = end;
-			str[high] = begin;
-			low++;
-			high--;
-		}
-	}	
+	
 }
 
 int main(){
